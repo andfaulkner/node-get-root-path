ec363140c2e638bf9e1cff60767a9e8d63a215a0
Andrew Faulkner <andrew.faulkner@cloudbeds.com>
2025-01-08 04:08:18 -0500

    Format code with prettier

--- index.ts
+++ index.ts
@@ -1,20 +1,18 @@
 /**************************************************************************************************
-*
-*    Current environments: server and build
-*
-*    Find the root path of the application i.e. the folder with package.json.
-*
-*/
+ *
+ *    Find the root path of the application i.e. the folder with a package.json file.
+ *
+ */
 
 /************************************** THIRD-PARTY IMPORTS ***************************************/
-import * as path from 'path';
-import * as fs from 'fs';
-import * as Promise from 'bluebird';
-import * as madLogs from 'mad-logs';
-import { path as appRootPath } from 'app-root-path';
+import * as path from "path";
+import * as fs from "fs";
+import * as Promise from "bluebird";
+import * as madLogs from "mad-logs";
+import { path as appRootPath } from "app-root-path";
 
 /**************************************** PROJECT IMPORTS *****************************************/
-const TAG = madLogs.buildFileTag('get-root-path.ts');
+const TAG = madLogs.buildFileTag("get-root-path.ts");
 
 const accessAsync = Promise.promisify(fs.access);
 
@@ -23,28 +21,27 @@ const accessAsync = Promise.promisify(fs.access);
  * Use when unable to resolve a path or find a file / directory
  */
 export class PathfinderError extends Error {
-    constructor(
-        public message: string,
-        public pathSeekerFile: string,
-        public pathSought: string
-    ) {
-        super(message);
-        this.message = `${pathSeekerFile} could not find ${pathSought}` +
-                       (message ? '. message' : '');
-        this.name = 'PathError';
-    }
+  constructor(
+    public message: string,
+    public pathSeekerFile: string,
+    public pathSought: string,
+  ) {
+    super(message);
+    this.message =
+      `${pathSeekerFile} could not find ${pathSought}` +
+      (message ? ". message" : "");
+    this.name = "PathError";
+  }
 }
 
 /********************************************* MODULE *********************************************/
 /**
  * Export the project root as-is
  */
-export const rootPath: string = (process.env.APP_ROOT_PATH
-                                    ? process.env.APP_ROOT_PATH
-                                    : appRootPath);
+export const rootPath: string = process?.env?.APP_ROOT_PATH ?? appRootPath;
 
 if (process.env.LOG_LEVEL === "silly") {
-    console.log(`${TAG} project root path: `, rootPath);
+  console.log(`${TAG} project root path: `, rootPath);
 }
 
 export default rootPath;
@@ -52,22 +49,21 @@ export default rootPath;
 /**
  * Find the project root.
  * If user set process.env.APP_ROOT_PATH, use that. If not found, traverse backwards from current
- * directory until a directory is found containing both node_modules and package.json.
+ * directory until a directory is found containing a package.json file.
  */
 export function getRootPathSync(): string {
-    if (process.env.APP_ROOT_PATH) {
-        return process.env.APP_ROOT_PATH;
+  if (process.env.APP_ROOT_PATH) {
+    return process.env.APP_ROOT_PATH;
+  }
+  return (function getRoot(dir: string) {
+    try {
+      const isPkgJson = fs.accessSync(path.join(dir, "./package.json"));
+    } catch (e) {
+      throwIfAtFsRoot(dir);
+      return getRoot(path.join(dir, ".."));
     }
-    return (function getRoot(dir: string) {
-        try {
-            const isPkgJson = fs.accessSync(path.join(dir, './package.json'));
-            const is_node_modules = fs.accessSync(path.join(dir, './node_modules'));
-        } catch (e) {
-            throwIfAtFsRoot(dir);
-            return getRoot(path.join(dir, '..'));
-        }
-        return dir;
-    }(path.join(__dirname, '../..')));
+    return dir;
+  })(path.join(__dirname, "../.."));
 }
 
 /**
@@ -76,22 +72,21 @@ export function getRootPathSync(): string {
  * change while running the app.
  */
 export function getRootPathAsync() {
-    if (process.env.APP_ROOT_PATH) {
-        return Promise.resolve(process.env.APP_ROOT_PATH);
-    }
-    return (function getRoot(dir: string) {
-        return accessAsync(path.join(dir, './package.json'))
-            .then(() => accessAsync(path.join(dir, './node_modules')))
-            .then(() => {
-                console.log(`${TAG} Found root path: `, dir);
-                return Promise.resolve(path.join(__dirname, '..'));
-            })
-            .catch((err: Error) => {
-                console.error(`${TAG} current path:`, dir);
-                throwIfAtFsRoot(dir);
-                return Promise.resolve(getRoot(path.join(dir, '..')));
-            });
-    }(path.join(__dirname, '..'))).then((dir) => dir);
+  if (process.env.APP_ROOT_PATH) {
+    return Promise.resolve(process.env.APP_ROOT_PATH);
+  }
+  return (function getRoot(dir: string) {
+    return accessAsync(path.join(dir, "./package.json"))
+      .then(() => {
+        console.log(`${TAG} Found root path: `, dir);
+        return Promise.resolve(path.join(__dirname, ".."));
+      })
+      .catch((err: Error) => {
+        console.error(`${TAG} current path:`, dir);
+        throwIfAtFsRoot(dir);
+        return Promise.resolve(getRoot(path.join(dir, "..")));
+      });
+  })(path.join(__dirname, "..")).then((dir) => dir);
 }
 
 /**
@@ -99,9 +94,11 @@ export function getRootPathAsync() {
  * @param  {string} dir - current directory (in the traversal process)
  */
 function throwIfAtFsRoot(dir: string): never | void {
-    if (dir === '/') {
-        throw new PathfinderError(
-            '', __filename, 'project root (package.json & node_modules location)'
-        );
-    } 
+  if (dir === "/") {
+    throw new PathfinderError(
+      "",
+      __filename,
+      "project root (package.json file location)",
+    );
+  }
 }
